1b9b7ddfd55d804b5ff6235ded7ee76460ac4cb1
danihak <91723296+danihak@users.noreply.github.com>
2023-03-18 21:09:49 +0530

    Delete data directory

--- data/PumpingStation_4.js
+++
@@ -1,1 +0,0 @@
-var json_PumpingStation_4 = {"type":"FeatureCollection","name":"PumpingStation_4","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"Type":"Pumping Station ","Name":"Pumping Station ","Latitude":25.32683,"Longitude":83.02987},"geometry":{"type":"Point","coordinates":[83.02987,25.32683]}}]}
