a9d36757bfc79960787de34a933e98e6c2eaf4ec
danihak <91723296+danihak@users.noreply.github.com>
2023-03-18 21:12:43 +0530

    Add files via upload

---
+++ data/OverheadTank_5.js
@@ -0,0 +1,1 @@
+var json_OverheadTank_5 = {"type":"FeatureCollection","name":"OverheadTank_5","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"Type":"Overhead Tank ","Name":"Overhead Tank ","Latitude":25.32936,"Longitude":83.02809},"geometry":{"type":"Point","coordinates":[83.02809,25.32936]}}]}
